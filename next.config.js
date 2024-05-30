bae84cfb1d047238e3f3efb1991c95220eefc761
Pierre Avital <pierre.avital@ditto.live>
2024-05-30 08:33:35 +0200

    Set the page as root page! :D

--- next.config.js
+++ next.config.js
@@ -1,10 +1,2 @@
-const pathPrefix = process.env.NODE_ENV === 'production'
-	? '/blog'
-	: '';
-
 module.exports = {
-	assetPrefix: pathPrefix,
-	env: {
-		pathPrefix,
-	},
 };
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,6 +3,6 @@ import Image from "next/image"
 
 export function Menu() {
 	return <div style={{ padding: "1em", display: "flex", alignItems: "center", justifyContent: "space-around" }}>
-		<Link href="/"><Image src="/images/logo-tiny.png" height={48} width={48} alt="Fox-shaped Home Button" title="Yes, this is a home button"></Image></Link>
+		<Link href="/"><Image src="/images/logo-tiny.png" height={48} width={48} alt="Home" title="Yes, this is a home button" /></Link>
 	</div>
 }
